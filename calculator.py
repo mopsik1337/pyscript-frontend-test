b866b0428bbebe9255dc5209cbfcb22e8f2750b6
Milan <76396495+mopsik1337@users.noreply.github.com>
2024-06-19 20:40:57 +0500

    run

---
+++ calculator.py
@@ -0,0 +1,32 @@
+
+print('First var?')
+a = input()
+print('second var?')
+b = input()
+
+print('what to do? + or - or / or * or **')
+c = input()
+
+if c == '+':
+    print(int(a) + int(b))
+else:
+    input
+if c == '-':
+    print(int(a) - int(b))
+else:
+    input
+if c == '/':
+    print(int(a) / int(b))
+else:
+    input
+if c == '*':
+    print(int(a) * int(b))
+else:
+    input
+if c == '**':
+    print(int(a) ** int(b))
+else:
+    input
+
+
+# to do: run this script on web :)
